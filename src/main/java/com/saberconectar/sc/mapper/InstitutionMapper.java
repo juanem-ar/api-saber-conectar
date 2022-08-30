433b1b90905a75005ab42ef0952cb0c0f2a6748a
Juan López <juanem@hotmail.com>
2022-08-29 21:36:16 -0300

    creando rama de trabajo

--- src/main/java/com/saberconectar/sc/mapper/InstitutionMapper.java
+++ src/main/java/com/saberconectar/sc/mapper/InstitutionMapper.java
@@ -1,28 +1,38 @@
 package com.saberconectar.sc.mapper;
 
+import com.saberconectar.sc.dto.CourseDTO;
 import com.saberconectar.sc.dto.InstitutionDTO;
-import com.saberconectar.sc.dto.StudentDTO;
 import com.saberconectar.sc.dto.UserDTO;
+import com.saberconectar.sc.entity.CourseEntity;
 import com.saberconectar.sc.entity.InstitutionEntity;
-import com.saberconectar.sc.entity.StudentEntity;
 import com.saberconectar.sc.entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 
 @Component
 public class InstitutionMapper {
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private CourseMapper courseMapper;
 
-    public InstitutionEntity institutionDTO2Entity(InstitutionDTO dto){
+    public InstitutionEntity institutionDTO2Entity(InstitutionDTO dto, Boolean loadCourses){
         InstitutionEntity entity = new InstitutionEntity();
         entity.setName(dto.getName());
         entity.setCuitNumber(dto.getCuitNumber());
         entity.setDeleted(dto.getDeleted());
         entity.setUserEntity(dto.getUserEntity());
+        if(loadCourses){
+            entity.setCourses(dto.getCourses());
+        }
         return entity;
     }
-    public InstitutionDTO institutionEntity2DTO(InstitutionEntity entity){
+    public InstitutionDTO institutionEntity2DTO(InstitutionEntity entity, Boolean loadCourses){
         InstitutionDTO dto = new InstitutionDTO();
         dto.setId(entity.getId());
         dto.setName(entity.getName());
@@ -30,8 +40,12 @@ public class InstitutionMapper {
         dto.setDeleted(entity.getDeleted());
         UserDTO userDTO = userMapper.userEntity2DTO(entity.getUserEntity(), false, false);
         UserEntity userEntity = userMapper.userDTO2Entity(userDTO);
-
         dto.setUserEntity(userEntity);
+        if (loadCourses){
+            Set<CourseDTO> dtos = courseMapper.courseEntitySet2DTOSet(entity.getCourses(), false);
+            Set<CourseEntity> entities = courseMapper.courseDTOSet2EntitySet(dtos, false);
+            dto.setCourses(entities);
+        }
 
 
         return dto;
@@ -48,5 +62,19 @@ public class InstitutionMapper {
         entity.setUserEntity(userEntity);
 
         return entity;
+    }
+    public List<InstitutionDTO> institutionEntityList2DTOList(List<InstitutionEntity> entities, boolean loadCourses){
+        List<InstitutionDTO> dtos = new ArrayList<>();
+        for(InstitutionEntity entity: entities){
+            dtos.add(this.institutionEntity2DTO(entity, loadCourses));
+        }
+        return dtos;
+    }
+    public List<InstitutionEntity> institutionDTOList2EntityList(List<InstitutionDTO> dtos, boolean loadCourses){
+        List<InstitutionEntity> entities = new ArrayList<>();
+        for(InstitutionDTO dto: dtos){
+            entities.add(institutionDTO2Entity(dto, loadCourses));
+        }
+        return entities;
     }
 }
---
+++ src/main/java/com/saberconectar/sc/service/CourseService.java
@@ -0,0 +1,14 @@
+package com.saberconectar.sc.service;
+
+import com.saberconectar.sc.dto.CourseDTO;
+
+
+public interface CourseService{
+    CourseDTO save(CourseDTO dto);
+    CourseDTO getCourseById(Long id);
+
+    CourseDTO update(Long id, CourseDTO dto);
+
+    void delete (Long id);
+    void isCorrect(Long id, String name);
+}
