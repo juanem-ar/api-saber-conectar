4e5a042b7c6421872a6967ee719d7224c50fd26e
Juan Manuel <juanemlopez@gmail.com>
2022-08-30 08:25:30 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 # api-saber-conectar
-
-circuito en funcionamiento:
-* creación usuario Institución.
-* creación curso.
-* creación usuario Estudiante.
-* inscripción a curso existente.
--- src/main/java/com/saberconectar/sc/service/StudentService.java
+++ src/main/java/com/saberconectar/sc/service/StudentService.java
@@ -4,8 +4,12 @@ import com.saberconectar.sc.dto.StudentDTO;
 
 public interface StudentService {
     StudentDTO studentRegister(StudentDTO dto);
-    void delete(Long id);
-    void isCorrect(Long id);
     StudentDTO getStudentById(Long id);
     StudentDTO update(Long id, StudentDTO dto);
+
+    void addCourse(Long idStudent, Long idCourse);
+    void removeCourse(Long idStudent, Long idCourse);
+    void delete(Long id);
+    void isCorrect(Long id);
+    void areCorrect(Long idOne, String nameOne, Long idTwo, String nameTwo);
 }
