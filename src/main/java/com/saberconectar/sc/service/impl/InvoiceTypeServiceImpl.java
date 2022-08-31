aac5382893ac54f48de99a22457db3985a877fc2
Juan López <juanem@hotmail.com>
2022-08-31 20:15:30 -0300

    implementando CRUD de invoice_types

--- src/main/java/com/saberconectar/sc/service/impl/InvoiceTypeServiceImpl.java
+++ src/main/java/com/saberconectar/sc/service/impl/InvoiceTypeServiceImpl.java
@@ -1,27 +1,49 @@
 package com.saberconectar.sc.service.impl;
 
 import com.saberconectar.sc.dto.InvoiceTypeDTO;
+import com.saberconectar.sc.entity.InvoiceTypeEntity;
+import com.saberconectar.sc.exception.ParamNotFound;
+import com.saberconectar.sc.mapper.InvoiceTypeMapper;
+import com.saberconectar.sc.repository.InvoiceTypeRepository;
 import com.saberconectar.sc.service.InvoiceTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
+
 @Service
 public class InvoiceTypeServiceImpl implements InvoiceTypeService {
-    @Override
+    @Autowired
+    private InvoiceTypeRepository invoiceTypeRepository;
+    @Autowired
+    private InvoiceTypeMapper invoiceTypeMapper;
+
     public List<InvoiceTypeDTO> getAllInvoiceTypes() {
-        return null;
+        List<InvoiceTypeEntity> entities = invoiceTypeRepository.findAll();
+        List<InvoiceTypeDTO> dtos = invoiceTypeMapper.invoiceTypeEntityList2DTOList(entities);
+        return dtos;
     }
-    @Override
     public InvoiceTypeDTO save(InvoiceTypeDTO type) {
-        return null;
+        InvoiceTypeEntity entity = invoiceTypeMapper.invoiceTypeDTO2Entity(type);
+        InvoiceTypeEntity entitySaved = invoiceTypeRepository.save(entity);
+        InvoiceTypeDTO result = invoiceTypeMapper.invoiceTypeEntity2DTO(entitySaved);
+        return result;
     }
-    @Override
     public InvoiceTypeDTO update(Long id, InvoiceTypeDTO type) {
-        return null;
+        isCorrect(id);
+        InvoiceTypeEntity entityId = invoiceTypeRepository.getReferenceById(id);
+        InvoiceTypeEntity entity = invoiceTypeMapper.update(entityId,type);
+        InvoiceTypeEntity entityUpdated = invoiceTypeRepository.save(entity);
+        InvoiceTypeDTO result = invoiceTypeMapper.invoiceTypeEntity2DTO(entityUpdated);
+        return result;
     }
-    @Override
     public void delete(Long id) {
+        isCorrect(id);
+        this.invoiceTypeRepository.deleteById(id);
     }
-    @Override
     public void isCorrect(Long id) {
+        if(!invoiceTypeRepository.existsById(id)){
+            throw new ParamNotFound("Invalid id");
+        }
     }
 }
